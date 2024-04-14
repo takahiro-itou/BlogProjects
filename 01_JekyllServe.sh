703b8d6d994dec261f2f6c616b6cceaf6b480d0b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-04-14 12:00:15 +0900

    補助スクリプトを修正
    (1) 受け取った引数をそのまま渡すように修正

--- 01_JekyllServe.sh
+++ 01_JekyllServe.sh
@@ -5,5 +5,5 @@ script_dir=$(readlink -f "$(dirname "$0")")
 blog_source_dir="${script_dir}/blog-pages/docs"
 
 pushd "${script_dir}/bundle-jekyll"
-./01_JekyllServe.sh  "${blog_source_dir}"
+./01_JekyllServe.sh  "${blog_source_dir}"  "$@"
 popd
